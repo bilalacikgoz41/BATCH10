1214a13b8b82ed83c7ed9cd8048c15fcf19cde37
Balakrishna Ette <balakrishna.ette@outlook.com>
2022-12-03 10:01:28 +0530

    HW

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,5 +1,10 @@
 import { LightningElement } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
-    
+    fullname = "Balakrishna";
+    age = 30;
+    location = {
+        city: "Houston",
+        country: "United States"
+    };
 }
